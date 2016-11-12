aaa03efbb19a165ae3a2284b043d5c296d1a4ef7
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-11-12 09:41:02 -0500

    Identify hand number with regex.

--- parser2.py
+++ parser2.py
@@ -1,8 +1,9 @@
-from pprint import pprint
+import re
 
 class Hand:
     def __init__(self, string):
         segments = "seats preflop flop turn river".split()
+        _hand_num_re = re.compile(r"^Ignition Hand #(\d+) .*$")
         self.seats = None
         self.preflop = None
         self.flop = None
@@ -15,11 +16,14 @@ class Hand:
             # We don't always have flop, turn, riv, but last element is
             # always Summary.
             k = segments.pop(0)
+            ## step 3: split each segment into lines
             v = s.pop(0).splitlines()
             self.__dict__[k] = v
-        ## step 3: split each segment into lines
         self.summary = s.pop(0).splitlines()
         assert len(s) == 0
+        ## step 4: parse various elements at sub-line level
+        match = _hand_num_re.match(self.seats[0])
+        self.hand_number = int(match.group(1))
     def __repr__(self):
         return str(self.__dict__)
 
