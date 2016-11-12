ff5c841ea776e3a7685ac68d276f509e5048709a
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-11-12 11:45:40 -0500

    Tidy up "main" portions.

--- parser2.py
+++ parser2.py
@@ -76,16 +76,15 @@ class Hand:
 
 
 ## main
-
-input = open('example_ignition.txt').read()
-
 ## step 1: split flat file into hands
+input = open('example_ignition.txt').read()
 hands = input.split('Ignition ')
 empty = hands.pop(0) # remove 1st always empty el
 assert empty == ''
-
 for i, h in enumerate(hands):
     hands[i] = Hand(h)
+
+## reporting
 
 for x in hands:
     print(x.hand_number)
